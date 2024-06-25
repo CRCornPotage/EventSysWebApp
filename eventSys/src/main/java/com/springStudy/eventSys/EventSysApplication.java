aed6998a1f35aea1ae8fa5f4f65d796791a55f9f
[Mirai] <subaru20011107@gmail.com>
2024-06-25 22:22:06 +0900

    プロジェクト作成

---
+++ eventSys/src/main/java/com/springStudy/eventSys/EventSysApplication.java
@@ -0,0 +1,13 @@
+package com.springStudy.eventSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSysApplication.class, args);
+	}
+
+}
